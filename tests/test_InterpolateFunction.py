49160b7e832f65b3c464d7f38f38271f59555755
bond-anton <bond.anton@gmail.com>
2020-03-26 21:55:06 +0300

    more tests for interpolation sub-module

--- tests/test_InterpolateFunction.py
+++ tests/test_InterpolateFunction.py
@@ -1,7 +1,8 @@
 import numpy as np
 from scipy.interpolate import interp1d
 
-from BDFunction1D.Interpolation import InterpolateFunction
+from BDMesh.Mesh1DUniform import Mesh1DUniform
+from BDFunction1D.Interpolation import InterpolateFunction, InterpolateFunctionMesh
 
 import unittest
 
@@ -15,6 +16,17 @@ class TestFunction(unittest.TestCase):
         x = np.linspace(0.0, 2 * np.pi, num=101, dtype=np.float)
         y = np.sin(x)
         f = InterpolateFunction(x, y)
+        x_new = np.linspace(0.0, 2 * np.pi, num=201, dtype=np.float)
+        f1 = interp1d(x, y, kind='linear')
+        np.testing.assert_allclose(f.evaluate(x_new), f1(x_new), atol=1e-12)
+
+    def test_interpolate_Function_Mesh(self):
+        x = np.linspace(0.0, 2 * np.pi, num=101, dtype=np.float)
+        y = np.sin(x)
+        mesh = Mesh1DUniform(0.0, 2.0 * np.pi, num=101)
+        mesh.solution = np.sin(x)
+        mesh.residual = np.ones_like(x) * 2
+        f = InterpolateFunctionMesh(mesh)
         x_new = np.linspace(0.0, 2 * np.pi, num=201, dtype=np.float)
         f1 = interp1d(x, y, kind='linear')
         np.testing.assert_allclose(f.evaluate(x_new), f1(x_new), atol=1e-12)
